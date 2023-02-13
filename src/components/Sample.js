5a88ed6f5cc1bdd120e0014979de94a7288d48b0
Arjun2002tiwari <Arjarjun200@gmail.com>
2023-02-13 15:47:18 +0530

    changes

---
+++ src/components/Sample.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class Sample extends Component{
+    render(){
+        return(
+            <div>hello world!</div>
+        )
+    }
+}
+export default Sample
